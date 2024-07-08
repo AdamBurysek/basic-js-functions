293dd759c9b248c275102d2cc89cb73d54db88a6
Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>
2024-07-08 17:02:44 +0200

    Add Every, Includes, Sort

---
+++ every.js
@@ -0,0 +1,4 @@
+const array1 = [1, 30, 39, 29, 10, 13];
+
+// Check if everey value meet rules 
+console.log(array1.every(currentValue => currentValue < 40)); // Output: true
---
+++ includes.js
@@ -0,0 +1,5 @@
+const array1 = [1, 2, 3];
+console.log(array1.includes(2)); // Output: true
+
+const pets = ['cat', 'dog', 'bat'];
+console.log(pets.includes('racoon')); // Output: false
